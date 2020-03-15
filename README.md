4e3f4be94324454d2abb2d32bcee18e037886f29
nashi2603 <nashi.20306.y@gmail.com>
2020-03-16 03:23:03 +0900

    init commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# RewardSender
+
+A Minecraft spigot server plugin.
+
+Any rewards give player.
---
+++ src/main/kotlin/com/github/nashi2603/rewardsender/RewardSender.kt
@@ -0,0 +1,13 @@
+package com.github.nashi2603.rewardsender
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class RewardSender : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
